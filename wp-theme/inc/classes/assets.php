d582adae76cd6cc65f5458013f6004fd2cdde8bb
Mohamed A. Eljaouadi <eljaouadi1mohamed@gmail.com>
2024-07-03 19:33:01 +0100

    📌 WP-Dev: Bootstrap file for classes

---
+++ wp-theme/inc/classes/assets.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ *
+ * Assets File
+ * Description: Register the frontend scripts & styles
+ * @package wpstartertheme
+ *
+ */
+
+class Asset
+{
+
+    protected function __construct()
+    {
+
+        $this->setup_hooks();
+    }
+
+    protected function setup_hooks()
+    {
+
+        // FILTERS & ACTIONS
+        add_action('wp_enqueue_scripts', [$this, 'load_styles']);
+        add_action('wp_enqueue_scripts', [$this, 'load_scripts']);
+    }
+
+    public function load_styles()
+    {
+
+        wp_register_style('bulma', THEME_CSS_DIR . '/assets/bulma.css', [], false, 'all');
+        wp_register_style('lineawesome', THEME_FONTS_DIR . '/lineawesome/all.css', [], false, 'all');
+        wp_register_style('core', THEME_CSS_DIR . '/core.css', ['bulma'], filemtime(THEME_CSS_DIR . '/core.css'), 'all');
+
+        wp_enqueue_style('bulma');
+        wp_enqueue_style('lineawesome');
+        wp_enqueue_style('core');
+    }
+
+    public function load_scripts()
+    {
+
+        wp_register_script('script', THEME_JS_DIR . '/app.js', [], filemtime(THEME_JS_DIR . '/app.js'), 'all');
+
+        wp_enqueue_script('script');
+
+        // SUPPORT IE VERSIONS < 9
+        wp_register_script('html5shiv', THEME_JS_DIR . '/assets/html5shiv.min.js', [], false, false);
+        wp_register_script('respond', THEME_JS_DIR . '/assets/respond.min.js', [], false, false);
+
+
+        wp_enqueue_script('html5shiv');
+        wp_enqueue_script('respond');
+
+        wp_script_add_data('html5shiv', 'conditional', 'lt IE 9');
+        wp_script_add_data('respond', 'conditional', 'lt IE 9');
+    }
+}
---
+++ wp-theme/inc/classes/bootstrap.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ *
+ * Bootstraps the theme
+ * Description: initializes the theme by setting up essential theme classes
+ * @package wpstartertheme
+ *
+ */
+
+namespace WST_THEME\Inc\Classes;
+
+use WST_THEME\Inc\Traits\Singleton;
+
+class Bootstrap
+{
+
+    use Singleton;
+
+    protected function __construct()
+    {
+
+        // LOAD CLASSES
+
+
+        // ADD ACTIONS & FILTERS FUNCTION
+        $this->setup_hooks();
+    }
+
+    protected function setup_hooks()
+    {
+
+        // ACTIONS & FILTERS
+        add_action('after_setup_theme', [$this, 'setup_theme']);
+    }
+
+    // ADD THEME SUPPORT FEATURES FUNCTION
+    public function setup_theme()
+    {
+
+        add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+        add_theme_support('widgets');
+        add_theme_support('align-wide');
+        add_theme_support('customize-selective-refresh-widgets');
+        add_theme_support('automatic-feed-links');
+        add_theme_support('html5',  ['comment-list', 'comment-form', 'search-form', 'gallery', 'caption', 'style', 'script']);
+        add_theme_support('wp-block-styles');
+        add_theme_support('custom-logo', ['header-text' => ['site-title', 'site-description'], 'height' => 'auto', 'width' => '100%', 'flex-height' => true, 'flex-width' => true]);
+        add_theme_support('custom-background', ['default-color' => '#FFF', 'default-image' => '', 'default-repeat' => 'no-repeat']);
+        add_image_size('featured-thumbnail', 353, 233, true);
+        remove_theme_support('core-block-patterns');
+        add_theme_support('woocommerce');
+    }
+}

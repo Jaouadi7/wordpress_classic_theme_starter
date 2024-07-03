083812c0d0b17c8a54350599134cd3dc555f6a41
Mohamed A. Eljaouadi <eljaouadi1mohamed@gmail.com>
2024-07-03 19:34:59 +0100

    📌 WP-Dev: Include Constants & autolder files + get instance bootstrap class

--- wp-theme/functions.php
+++ wp-theme/functions.php
@@ -5,3 +5,14 @@
  * @package wpstartertheme
  *
  */
+
+require_once(untrailingslashit(get_template_directory() . '/inc/constants.php'));
+require_once(untrailingslashit(get_template_directory() . '/inc/helpers/autoloader.php'));
+
+// THEME CLASSES
+function get_theme_instance()
+{
+    \WST_THEME\Inc\Classes\Bootstrap::get_instance();
+}
+
+get_theme_instance();

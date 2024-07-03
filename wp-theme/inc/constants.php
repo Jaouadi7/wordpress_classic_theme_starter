70a8234d24d48b2974f7261ee49589f707d917bd
Mohamed A. Eljaouadi <eljaouadi1mohamed@gmail.com>
2024-07-03 19:35:50 +0100

    📌 WP-Dev: Add Constants variables

--- wp-theme/inc/constants.php
+++ wp-theme/inc/constants.php
@@ -7,3 +7,8 @@
  * @package wpstartertheme
  *
  */
+
+! defined('THEME_CSS_DIR') && define('THEME_CSS_DIR', untrailingslashit(get_template_directory_uri() . '/frontend/css'));
+! defined('THEME_JS_DIR') && define('THEME_JS_DIR', untrailingslashit(get_template_directory_uri() . '/frontend/js'));
+! defined('THEME_FONTS_DIR') && define('THEME_FONTS_DIR', untrailingslashit(get_template_directory_uri() . '/frontend/fonts'));
+! defined('THEME_IMAGES_DIR') && define('THEME_IMAGES_DIR', untrailingslashit(get_template_directory_uri() . '/frontend/imgs'));

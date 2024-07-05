885f8cd581028b133901d16c1c55f37667825b7c
Mohamed A. Eljaouadi <eljaouadi1mohamed@gmail.com>
2024-07-05 14:11:53 +0100

    📌 WP-Dev: create archive  template

---
+++ wp-theme/archive.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Archive template
+ * Description: The archive template is used when visitors request posts by category, author, or date.
+ * @package wpstartertheme
+ *
+ */
+
+get_header();
+
+
+get_footer();

08d3c5cb4c952202aa568b2c362e7cbf0fb6f09a
Mohamed A. Eljaouadi <eljaouadi1mohamed@gmail.com>
2024-07-05 14:12:13 +0100

    📌 WP-Dev: create attachments  template

---
+++ wp-theme/attachment.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Attachment template
+ * Description: used when viewing a single attachment like an image, pdf, or other media file.
+ * @package wpstartertheme
+ *
+ */
+
+get_header();
+
+
+get_footer();

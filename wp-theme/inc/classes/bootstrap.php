602307e11d1545aaa16acf075f6d5f8920f7d2bd
Mohamed A. Eljaouadi <eljaouadi1mohamed@gmail.com>
2024-07-09 13:48:40 +0100

    📌 WP-Dev: load widgets class to register custom widgets

--- wp-theme/inc/classes/bootstrap.php
+++ wp-theme/inc/classes/bootstrap.php
@@ -24,6 +24,7 @@ class Bootstrap
         Menus::get_instance();
         Woocommerce::get_instance();
         Sidebars::get_instance();
+        Widgets::get_instance();
         CPT::get_instance();
         Taxonomies::get_instance();
         Metaboxs::get_instance();

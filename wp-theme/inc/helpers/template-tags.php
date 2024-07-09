b7025700fafa94b567f19fbb83c498602a6796e2
Mohamed A. Eljaouadi <eljaouadi1mohamed@gmail.com>
2024-07-09 13:53:46 +0100

    📌 WP-Dev: create loop post functions [add more & add excepert]

--- wp-theme/inc/helpers/template-tags.php
+++ wp-theme/inc/helpers/template-tags.php
@@ -8,8 +8,12 @@
  */
 
 // Custom post thumbnail with lazy loading attribute
-function get_the_post_custom_thumbnail(int $post_id = get_the_ID(),  string $size = 'featured-image', array $additional_attr = [])
+function theme_get_the_post_thumbnail(int|null $post_id = null,  string $size = 'featured-image', array $additional_attr = [])
 {
+
+    if ($post_id === null) {
+        $post_id = get_the_ID();
+    }
 
     $custom_the_post_thumbnail = '';
 
@@ -26,3 +30,27 @@ function get_the_post_custom_thumbnail(int $post_id = get_the_ID(),  string $siz
 
     return $custom_the_post_thumbnail;
 }
+
+// get the excerpt [ loop posts ]
+function theme_get_the_post_excerpt(int $length = 50)
+{
+
+    $excerpt = wp_strip_all_tags(get_the_excerpt());
+    $excerpt = substr($excerpt, 0, $length);
+    $excerpt = substr($excerpt, 0, strpos($excerpt, '')) . '[...]';
+
+    return $excerpt;
+}
+
+// More read button [ loop posts ]
+function theme_get_the_post_read_more_btn()
+{
+
+    $read_more = sprintf('<a class="button read-more-btn" href="%1$s">%2$s</a>', get_permalink(get_the_ID()), __('Read More', 'wpstartertheme'));
+
+    return $read_more;
+}
+
+
+// Pagination 
+function theme_pagination() {}

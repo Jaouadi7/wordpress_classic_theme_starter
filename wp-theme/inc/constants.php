5b0c1de7938c14723238e2aeee1f1bba718efe4b
Mohamed A. Eljaouadi <eljaouadi1mohamed@gmail.com>
2024-07-15 11:59:07 +0100

    add variables for env mode

--- wp-theme/inc/constants.php
+++ wp-theme/inc/constants.php
@@ -8,7 +8,8 @@
  *
  */
 
-! defined('THEME_CSS_DIR') && define('THEME_CSS_DIR', untrailingslashit(get_template_directory_uri() . '/frontend/css'));
-! defined('THEME_JS_DIR') && define('THEME_JS_DIR', untrailingslashit(get_template_directory_uri() . '/frontend/js'));
-! defined('THEME_FONTS_DIR') && define('THEME_FONTS_DIR', untrailingslashit(get_template_directory_uri() . '/frontend/fonts'));
-! defined('THEME_IMAGES_DIR') && define('THEME_IMAGES_DIR', untrailingslashit(get_template_directory_uri() . '/frontend/imgs'));
+! defined('DEVELEPMENT_MODE') && define('DEVELEPMENT_MODE', TRUE);
+! defined('THEME_CSS_DIR') && define('THEME_CSS_DIR', untrailingslashit(get_template_directory_uri() . '/frontend/dist/css'));
+! defined('THEME_JS_DIR') && define('THEME_JS_DIR', untrailingslashit(get_template_directory_uri() . '/frontend/dist/js'));
+! defined('THEME_FONTS_DIR') && define('THEME_FONTS_DIR', untrailingslashit(get_template_directory_uri() . '/frontend/dist/fonts'));
+! defined('THEME_IMAGES_DIR') && define('THEME_IMAGES_DIR', untrailingslashit(get_template_directory_uri() . '/frontend/dist/imgs'));
